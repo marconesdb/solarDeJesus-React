501977ccb257cb89c3a4736835b9d3ad884ca4eb
marconesb <marconebritt@gmail.com>
2024-02-15 20:13:55 -0300

    Correções da Galeria

--- src/pages/quemsomos/Galeria.jsx
+++ src/pages/quemsomos/Galeria.jsx
@@ -5,9 +5,6 @@ import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import imagem1 from '../../public/Galeria/Biblioteca/WhatsApp Image 2024-02-07 at 19.32.39(1).jpeg';
 import imagem2 from '../../public/Galeria/Biblioteca/WhatsApp Image 2024-02-07 at 19.32.40(1).jpeg';
-import img1 from '../../public/Galeria/Biblioteca/WhatsApp Image 2024-02-07 at 19.32.41(1).jpeg'
-import img2 from '../../public/Galeria/Biblioteca/WhatsApp Image 2024-02-07 at 19.32.42(1).jpeg'
-import img3 from '../../public/Galeria/Biblioteca/WhatsApp Image 2024-02-07 at 19.32.42.jpeg'
 
 const images1 = [
     {
@@ -20,29 +17,11 @@ const images1 = [
         thumbnail: imagem2,
         description: 'Biblioteca'
     }
-    // Adicione mais imagens conforme necessário
 ];
 
-// const images2 = [
-//     {
-//         original: img1,
-//         thumbnail: img1,
-//         description: 'Salão de Palestras'
-//     },
-
-//     {
-//       original: img2,
-//       thumbnail: img2,
-//       description: 'Biblioteca'
-//   },
-
-//   {
-//     original: img3,
-//     thumbnail: img3,
-//     description: 'Biblioteca'
-// },
-//     // Adicione mais imagens conforme necessário
-// ];
+const images2 = [
+    // Defina as imagens para a segunda galeria aqui, se necessário
+];
 
 const Galeria = () => {
     return (
@@ -56,6 +35,8 @@ const Galeria = () => {
                 <div className="hidden sm:block"> {/* Visível apenas em dispositivos desktop */}
                     <ImageGallery items={images1} />
                 </div>
+
+                {/* Adicione aqui a segunda galeria de fotos, se necessário */}
 
                 <h2 className="text-2xl font-bold my-8"></h2>
                 <div className="sm:hidden"> {/* Visível apenas em dispositivos móveis */}
