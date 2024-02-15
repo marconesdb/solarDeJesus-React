d45c791cf73fd3c7a7c33e03b9dfcd83d4c4661a
marconesb <marconebritt@gmail.com>
2024-02-15 15:30:33 -0300

    Correção de Bug no Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="bg-white text-gray-400 py-8">
-      <div className="container mx-auto flex flex-col items-center md:flex-row md:justify-between">
-        <p className="text-center md:text-left">
+      <div className="container mx-auto md:flex md:items-center md:justify-between px-4">
+        <p className="text-center md:text-left md:ml-4 mb-4 md:mb-0">
           &copy; Instituição Espírita de Caridade Solar de Jesus - 2024 Todos os direitos reservados
         </p>
-        <div className="mt-4 md:mt-0">
-          <p className="text-center md:text-left">
+        <div className="text-center md:text-left">
+          <p className="mb-2">
             Endereço: Av. Leonel Beirão de Jesus, 1555, Bairro: Sumaré
             <br />
             CEP: 39402-547 - Montes Claros - MG/BR
           </p>
-          <p className="text-center md:text-left">CPNJ 21.355.599/0001-56</p>
+          <p>CPNJ 21.355.599/0001-56</p>
         </div>
       </div>
     </footer>
