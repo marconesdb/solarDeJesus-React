3097dfdbf94dcbe11604a1c4b78b8c07a9ed4e61
marconesb <marconebritt@gmail.com>
2024-02-16 13:14:40 -0300

    Correçaõ pág Home mobile

--- src/pages/home/Carousel.jsx
+++ src/pages/home/Carousel.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import image1 from './img-carousel/Captura de Tela (4035).png'
-import image2 from './img-carousel/Captura de Tela (4036).png'
-import image3 from './img-carousel/Captura de Tela (4037).png'
-export default function Carousel() {
-  const images = [
-    image1,
-    image2,
-    image3,
-  ];
+import image1 from './img-carousel/Captura de Tela (4035).png';
+import image2 from './img-carousel/Captura de Tela (4036).png';
+import image3 from './img-carousel/Captura de Tela (4037).png';
 
+export default function Carousel() {
+  const images = [image1, image2, image3];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const nextImage = () => {
@@ -30,10 +26,10 @@ export default function Carousel() {
   }, [currentImageIndex]);
 
   return (
-    <div className="container mx-auto">
-      <div className="relative">
+    <div className="container mx-auto h-screen flex items-start justify-center pt-10 ">
+      <div className="relative h-96 w-full">
         <button
-          className="absolute top-1/2 left-0 transform -translate-y-1/2  text-white px-3 py-1 rounded-l focus:outline-none"
+          className="absolute top-1/2 left-0 transform -translate-y-1/2 text-white px-3 py-1 rounded-l focus:outline-none"
           onClick={prevImage}
         >
           
@@ -44,7 +40,11 @@ export default function Carousel() {
         >
           
         </button>
-        <img src={images[currentImageIndex]} alt={`Slide ${currentImageIndex + 1}`} className="w-full" />
+        <img
+          src={images[currentImageIndex]}
+          alt={`Slide ${currentImageIndex + 1}`}
+          className="w-full h-full object-cover mt-20"
+        />
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,15 +4,7 @@ import  Footer from '../../Footer'
 import Carousel from './Carousel'
 import bannerVoluntariosDaAlegria from '../quemsomos/img-nossostrabalhos/Voluntários da Alegria.jpeg'
 import Modal from '../home/Modal'
-// import img1 from '../img-cartaz-palestras/1.png'
-// import img2 from '../img-cartaz-palestras/2.png'
-// import img3 from '../img-cartaz-palestras/3.png'
-// import img4 from '../img-cartaz-palestras/Cartaz-1.png'
-// import img5 from '../img-cartaz-palestras/Cartaz-2.png'
-// import img6 from '../img-cartaz-palestras/Cartaz-3.png'
-// import img7 from '../img-cartaz-palestras/Cartaz-04.png'
-// import img8 from '../img-cartaz-palestras/Cartaz-05.png'
-// import img9 from '../img-cartaz-palestras/Cartaz-07.png'
+
 export default function Home() {
 
   
@@ -21,17 +13,17 @@ export default function Home() {
       <Navbar/>
       <Carousel/>
       {/* Banner Voluntários da Alegria */}
-      <div className="container mx-auto px-4 py-8 md:flex md:items-center md:justify-between">
+      <div className="container mx-auto px-4 py-8 md:flex md:items-center md:justify-between   -mt-96">
       <div className="md:w-1/2 md:pr-4 mb-4 md:mb-0">
         <img
-          className="object-cover object-center w-full h-64 md:h-auto md:w-full"
+          className=" object-center w-full h-64 md:h-auto md:w-full"
           src={bannerVoluntariosDaAlegria}
           alt="Imagem"
         />
       </div>
       <div className="md:w-1/2 md:pl-4  bg-orange500 text-white rounded-bl-3xl rounded-tr-3xl">
         <h2 className="text-2xl font-bold mb-4">"Seja um Voluntário da Alegria:</h2>
-        <p className="text-lg">
+        <p className=" ml-4 text-lg flex justify-center items-center">
         ofereça seu amor ao próximo, doando seu tempo e proporcionando consolo moral e espiritual." 
         </p>
       </div>
